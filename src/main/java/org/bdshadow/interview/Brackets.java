b07899149f17fb5ea7f4788720a4a8f9bd48d9fd
Max Vorobyev <gtvolk31@gmail.com>
2021-04-02 19:18:08 +0300

    Fix

--- src/main/java/org/bdshadow/interview/Brackets.java
+++ src/main/java/org/bdshadow/interview/Brackets.java
@@ -1,9 +1,34 @@
 package org.bdshadow.interview;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Brackets {
 
+
     public boolean isCorrect(String s) {
-        // TODO
-        return true;
+        HashMap<String, String> finiteState = new HashMap<>();
+        finiteState.put("(", ")");
+
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            String character = String.valueOf(s.charAt(i));
+            if (!stack.contains(character)) {
+                String next = finiteState.get(character);
+                if (next != null) {
+                    switch (next) {
+                        case "TERMINATOR": continue;
+                        default:
+                            stack.push(next);
+                            continue;
+                    }
+                }
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
